743d28c8a101959672bd86d7d4b32dcddb872d65
Sesha <seshasatyasai2003@gmail.com>
2024-12-13 13:32:42 +0530

    refreshed login

--- admin_dashboard.py
+++ admin_dashboard.py
@@ -5,4 +5,11 @@ Builder.load_file('kv/admin_dashboard.kv')
 
 
 class AdminDashboardScreen(Screen):
-    pass
+    def logout(self):
+        # Clear the email and password fields before navigating to login
+        login_screen = self.manager.get_screen('login')
+        login_screen.ids.username.text = ''
+        login_screen.ids.password.text = ''
+        
+        # Navigate to the login screen
+        self.manager.current = 'login'
