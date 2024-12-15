6c5c2db593764bd393e88e303d3d6249af0e25a1
ramya765 <132564203+ramya765@users.noreply.github.com>
2024-12-16 01:17:53 +0530

    Removed all files from master branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# StreamSmart_app
-An innovative platform that enables secure and transparent sharing of premium streaming subscriptions, making high-quality content more affordable and accessible by dividing costs among multiple users.
--- image_button.py
+++
@@ -1,9 +0,0 @@
-from kivy.lang import Builder
-from kivy.uix.image import Image
-from kivy.uix.behaviors import ButtonBehavior
-
-# Load the KV file from the kv folder
-Builder.load_file('kv/image_button.kv')
-
-class ImageButton(ButtonBehavior, Image):
-    pass
